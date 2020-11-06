a3a0c1026f8053f8fda783db1fd2817dfed14395
ailtonbsj <ailton.ifce@gmail.com>
2020-11-06 01:55:06 -0300

    Added a side menu

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,42 @@
 <template>
   <v-app id="vuetify-demo">
-    <v-navigation-drawer v-model="drawer" app></v-navigation-drawer>
-    <v-app-bar app>
+    <v-navigation-drawer v-model="drawer" app>
+      <v-list>
+        <v-list-item class="px-2">
+          <v-list-item-avatar size="64">
+            <v-img
+              src="https://randomuser.me/api/portraits/women/10.jpg"
+            ></v-img>
+          </v-list-item-avatar>
+        </v-list-item>
+
+        <v-list-item link>
+          <v-list-item-content>
+            <v-list-item-title class="title"> Admin </v-list-item-title>
+            <v-list-item-subtitle>admin@local</v-list-item-subtitle>
+          </v-list-item-content>
+        </v-list-item>
+      </v-list>
+      <v-divider></v-divider>
+      <v-list nav dense>
+        <v-list-item link>
+          <v-list-item-icon>
+            <v-icon>mdi-home</v-icon>
+          </v-list-item-icon>
+          <v-list-item-title>Home</v-list-item-title>
+        </v-list-item>
+        <v-list-item link>
+          <v-list-item-icon>
+            <v-icon>mdi-information-outline</v-icon>
+          </v-list-item-icon>
+          <v-list-item-title>About</v-list-item-title>
+        </v-list-item>
+      </v-list>
+    </v-navigation-drawer>
+    <v-app-bar color="green" app>
       <v-app-bar-nav-icon @click="drawer = !drawer"></v-app-bar-nav-icon>
       <v-toolbar-title>Vuetify demo</v-toolbar-title>
     </v-app-bar>
-
     <v-main> </v-main>
   </v-app>
 </template>
